328924ba0942e19df6ed7b1f00d45fc62277e19f
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r13s9.42antananarivo.mg>
2024-05-31 17:59:14 +0300

    Rank00

---
+++ Rank00/libft/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: torandri <torandri@student.42antanana      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 12:39:35 by torandri          #+#    #+#             */
+/*   Updated: 2024/02/22 13:18:38 by torandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
